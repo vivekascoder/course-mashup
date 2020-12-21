20806b7591a5cc649bc2a16a45cbba299e87fbc1
vivekascoder <vivekascoder@gmail.com>
2020-12-21 18:37:23 +0530

    Added my emai and password for hacking

--- course_mash/settings.py
+++ course_mash/settings.py
@@ -149,8 +149,8 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "coursemashup@gmail.com"
+EMAIL_HOST_PASSWORD = "heycoursemashup@gmail"
 print(EMAIL_HOST_USER)
 print(EMAIL_HOST_PASSWORD)
 
